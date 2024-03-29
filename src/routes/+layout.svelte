c52e86c3aff133310bca0a48e29b6b69148e1085
slylealdrich <slylealdrich@gmail.com>
2024-03-28 23:56:14 -0500

    added tailwind and working on nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+    import "../app.css";
+</script>
+
+<nav class="grid grid-cols-5 grid-rows-1  bg-black h-16 text-white">
+    <a href="/" class="grid justify-center content-center hover:bg-gray-700">
+        <h1 class="text-2xl font-bold">
+            Home
+        </h1>
+    </a>
+    <a href="/profile" class="grid justify-center content-center font-bold hover:bg-gray-700">
+        <h1 class="text-2xl font-bold">
+            Profile
+        </h1>
+    </a>
+
+    <div class="col-start-5 grid justify-around content-center">
+        <button class="col-start-5 w-32 h-8 font-bold bg-amber-400 text-black rounded hover:bg-amber-300">
+            Sign In
+        </button>
+    </div>
+</nav>
+
+<slot />
